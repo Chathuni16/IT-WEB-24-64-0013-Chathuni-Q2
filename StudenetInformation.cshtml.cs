b86eead578a0931eb78807e9d3fefdc98903c3f9
Chathuni16 <chathuabeysinghe16@gmail.com>
2024-10-27 00:49:56 +0530

    Add files via upload

---
+++ StudenetInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentsManagementApp.Pages
+{
+    public class StudenetInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
